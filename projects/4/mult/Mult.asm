2727b4efbde8149c5388335ef15b7093c26ce325
David Puerta Martos <dpuerta@usal.es>
2025-12-14 14:37:58 +0100

    project 4 - mult.asm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,3 +7,29 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+@R2
+M=0
+
+(START_LOOP)
+    @R1
+    D=M
+    @END_LOOP
+    D;JEQ
+
+    // ADD R0 TO R2
+    @R0
+    D=M        // Get R0
+    @R2
+    M=D+M      // R2 = R0 + R2
+
+    // DECREMENT R1
+    @R1
+    M=M-1
+
+    // REPEAT
+    @START_LOOP
+    0;JMP
+
+(END_LOOP)
+    @END_LOOP
+    0;JMP
